5344d6e9da2ce09bc0eeae9b3a18beed759909d0
Venkatesh <vkalva45@gmail.com>
2019-10-06 12:20:39 -0500

    Create a column chart controller.

---
+++ Google-charts-api/Models/PopulationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Google_charts_api.Models
+{
+    public class PopulationModel
+    {
+        public string CityName { get; set; }
+        public int PopulationYear2020 { get; set; }
+        public int PopulationYear2010 { get; set; }
+        public int PopulationYear2000 { get; set; }
+        public int PopulationYear1990 { get; set; }
+    }
+}
