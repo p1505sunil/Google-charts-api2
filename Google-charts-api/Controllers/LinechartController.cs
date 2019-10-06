f380780f0703c57e91c7d19b7fb643d9b76d0384
Venkatesh <vkalva45@gmail.com>
2019-10-06 12:43:42 -0500

    create a linechart controller.

---
+++ Google-charts-api/Controllers/LinechartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Google_charts_api.Controllers
+{
+    public class LinechartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult PopulationChart()
+        {
+            var populationList = PopulationDataAccessaLayer.GetCityPopulationList();
+            return Json(populationList);
+        }
+    }
+}
